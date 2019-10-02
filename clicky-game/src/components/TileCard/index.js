8661ea0a0bfe0b7b1d4a36da3a4f43fb1c5b1e04
Ryan Ward <ryanward4556@gmail.com>
2019-10-02 16:34:28 -0400

    knows when I click a card, but does not switch state from false to true

--- clicky-game/src/components/TileCard/index.js
+++ clicky-game/src/components/TileCard/index.js
@@ -1,21 +1,39 @@
-import React from 'react';
+import React, { Component } from 'react';
 import "./style.css";
 import Card from 'react-bootstrap/Card';
+// import { ToggleButton } from 'react-bootstrap';
 
 
-function TileCard(props) {
+class TileCard extends Component {
 
-    return (
+    state = {
+        active: false
+    }
 
-        <Card style={{ width: '18rem' }}>
-            <Card.Img variant="top" src={props.image} />
-            <Card.Body>
-                <Card.Title>{props.name}</Card.Title>
-            </Card.Body>
-        </Card >
+    handleToggle(e) {
+        console.log(this.state.active)
 
-    )
+        const currentState = this.state.active;
+        this.setState({
+            active: !currentState
+        });
+        console.log(this.state.active)
+    }
 
+    render() {
+        return (
+
+            <Card toggle={this.state.active ? "toggle" : null}
+                onClick={this.handleToggle.bind(this)}
+            >
+                <Card.Img variant="top" src={this.props.image} />
+                <Card.Body>
+                    <Card.Title>{this.props.name}</Card.Title>
+                </Card.Body>
+            </Card >
+
+        )
+    }
 }
 
 export default TileCard;
