b5e059e838f95903b5ad3773fa637f1c5b1c3269
Ryan Ward <ryanward4556@gmail.com>
2019-10-10 09:43:19 -0400

    Updates

---
+++ precache-manifest.a97881d247c83ec70bb1cb59aa533e34.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff7c871a8097d99962f50061d854ed92",
+    "url": "/Clicky-Game/index.html"
+  },
+  {
+    "revision": "48c906c0d6359949b48b",
+    "url": "/Clicky-Game/static/css/2.7c4efcaa.chunk.css"
+  },
+  {
+    "revision": "2cab82868a3722cedc3c",
+    "url": "/Clicky-Game/static/css/main.0b8e7058.chunk.css"
+  },
+  {
+    "revision": "48c906c0d6359949b48b",
+    "url": "/Clicky-Game/static/js/2.082673bf.chunk.js"
+  },
+  {
+    "revision": "2cab82868a3722cedc3c",
+    "url": "/Clicky-Game/static/js/main.fcaa88e0.chunk.js"
+  },
+  {
+    "revision": "6a0ee9095a2e4512de33",
+    "url": "/Clicky-Game/static/js/runtime-main.ef072257.js"
+  },
+  {
+    "revision": "327957d064f4e5d6175453aeb30ca5f8",
+    "url": "/Clicky-Game/static/media/allison.327957d0.png"
+  },
+  {
+    "revision": "295274c075cff1f1ffb36215d47454ae",
+    "url": "/Clicky-Game/static/media/deligt.295274c0.png"
+  },
+  {
+    "revision": "8ae5393d3a423e58edd78d468c135558",
+    "url": "/Clicky-Game/static/media/frenkie.8ae5393d.png"
+  },
+  {
+    "revision": "a01051b001629502726a93a5f58b792c",
+    "url": "/Clicky-Game/static/media/hazard.a01051b0.jpg"
+  },
+  {
+    "revision": "c7f0857df7aaf9abbcb85dd76204e7e4",
+    "url": "/Clicky-Game/static/media/mbappe.c7f0857d.png"
+  },
+  {
+    "revision": "a5215017b1e7ebecf8f0978de51c29dc",
+    "url": "/Clicky-Game/static/media/messi.a5215017.png"
+  },
+  {
+    "revision": "1524c8c50fc2b155b5fb0046013ebc57",
+    "url": "/Clicky-Game/static/media/modric.1524c8c5.png"
+  },
+  {
+    "revision": "de0d4e9d526052c59829b7746c4c5936",
+    "url": "/Clicky-Game/static/media/ramos.de0d4e9d.png"
+  },
+  {
+    "revision": "ed48c9d2239e114b762f64bcf1ebdc40",
+    "url": "/Clicky-Game/static/media/ronaldo.ed48c9d2.png"
+  },
+  {
+    "revision": "c266120dad598d26ee50f8fa598e620a",
+    "url": "/Clicky-Game/static/media/vandijk.c266120d.jpg"
+  }
+]);
